afc98987045cd3ea56c583efa7efb96700e087d8
Macintosh HD <otimusblue@gmail.com>
2019-03-25 09:37:18 +0700

    Son

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Config.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+
+public class Config {
+    public static final String FILE_UPLOAD_URL = "http://116.98.213.168:5005/getNoteText";
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
